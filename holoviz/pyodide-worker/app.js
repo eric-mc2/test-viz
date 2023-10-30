e1eefbd06ac909d4a3569c0a6fd57a424752380c
Eric Chandler <echandler@uchicago.edu>
2023-10-30 16:46:14 -0500

    mvp network viz

--- holoviz/pyodide-worker/app.js
+++ holoviz/pyodide-worker/app.js
@@ -15,7 +15,7 @@ async function startApplication() {
   self.pyodide.globals.set("sendPatch", sendPatch);
   console.log("Loaded!");
   await self.pyodide.loadPackage("micropip");
-  const env_spec = ['https://cdn.holoviz.org/panel/wheels/bokeh-3.3.0-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.3.0/dist/wheels/panel-1.3.0-py3-none-any.whl', 'pyodide-http==0.2.1', 'holoviews', 'hvplot', 'networkx', 'numpy', 'pandas']
+  const env_spec = ['https://cdn.holoviz.org/panel/wheels/bokeh-3.3.0-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.3.0/dist/wheels/panel-1.3.0-py3-none-any.whl', 'pyodide-http==0.2.1', 'holoviews', 'numpy', 'pandas']
   for (const pkg of env_spec) {
     let pkg_name;
     if (pkg.endsWith('.whl')) {
@@ -49,52 +49,51 @@ init_doc()
 
 import numpy as np
 import pandas as pd
-import networkx as nx
 import holoviews as hv
-from holoviews import opts
 import panel as pn
-import hvplot.pandas
 
 hv.extension('bokeh')
 
 # nb: I've only had luck loading these from URLs. Not from local paths.
-# TODO: use requests to load the gml into memory to cut out this boilerplate.
 nodes = pd.read_csv("https://raw.githubusercontent.com/eric-mc2/test-viz/main/data/nodes.csv", index_col=False)
 edges = pd.read_csv("https://raw.githubusercontent.com/eric-mc2/test-viz/main/data/edges.csv", index_col=False)
 
-G = nx.DiGraph()
-for row in nodes.to_dict(orient='index').values():
-    rowdata = {k:v for k,v in row.items() if k != 'id'}
-    G.add_node(row['id'], **rowdata)
-
-for row in edges.to_dict(orient='index').values():
-    rowdata = {k:v for k,v in row.items() if k == 'count' or k == 'amount' or k == 'logamount'}
-    G.add_edge(row['source'], row['target'], **rowdata)
-
+# # # I think hv edges need an actual integer source and target
+# nodes.index.name = 'idx'
+# nodes = nodes.reset_index()
+# edges['source_idx'] = edges['source'].map(nodes.set_index('id')['idx'])
+# edges['target_idx'] = edges['target'].map(nodes.set_index('id')['idx'])
 
 # Reposition LCC and non-LCC so they're not overlapping
-x_max = max(dict(G.nodes('x')).values())
-x_min = min(dict(G.nodes('x')).values())
+x_min, x_max = nodes['x'].min(), nodes['x'].max()
 x_pad = (x_max - x_min) * .1
-for _, nodedata in G.nodes(data=True):
-    if nodedata['lcc'] == 0:
-        nodedata['x'] += x_max + x_pad
+nodes.loc[nodes['lcc'] == 0, 'x'] += x_max + x_pad
 
-# removing 'x' and 'y' attributes. they are reserved in Panel/Bokeh
-node_positions = {n: (d['x'],d['y']) for n,d in G.nodes(data=True)}
-for node,node_data in G.nodes(data=True):
-    del node_data['x']
-    del node_data['y']
+# Select properties to view
+nodes = nodes.drop(columns='category').rename(columns={'pretty_category':'category'})
+
+# Create holoviews graph
+# hv_nodes = hv.Nodes(nodes, kdims=['x','y','idx'], vdims=['name','category','state_abbrv'])
+# hv_graph = hv.Graph(((edges['source_idx'], edges['target_idx'],edges['amount']), hv_nodes), vdims='amount')
+hv_nodes = hv.Nodes(nodes, kdims=['x','y','id'], vdims=['name','category','state_abbrv'])
+hv_graph = hv.Graph(((edges['source'], edges['target'],edges['amount']), hv_nodes), vdims='amount')
 
 hv_opts = dict(
+    # Holoviews arrowheads are all messed up:
+    # https://github.com/holoviz/holoviews/issues/3562
+    # Even using arrowhead_length = .01 and aspect='equal'
+    # It's hard to tell an arrow vs an edge since it's not filled.
+    directed=False,
+    # Holoviews node sizes are zoom-invariant, which is annoying.
+    node_size=8, 
     tools=['hover'], 
     xaxis=None, 
     yaxis=None,
-    height=600,
-    width=800)
+    height=800,
+    width=960,
+    title="Police Foundation Donations")
 
-net = hv.Graph.from_networkx(G, positions=node_positions).opts(**hv_opts)
-pn.panel(net).servable()
+pn.panel(hv_graph.opts(**hv_opts)).servable()
 
 await write_doc()
   `
