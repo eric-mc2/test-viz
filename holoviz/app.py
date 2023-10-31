fbf8bd3fa01755ff972070f1e0b60e7cb221a861
Eric Chandler <echandler@uchicago.edu>
2023-10-31 17:31:00 -0500

    holoviz doesnt require integer indexes

--- holoviz/app.py
+++ holoviz/app.py
@@ -9,24 +9,16 @@ hv.extension('bokeh')
 nodes = pd.read_csv("https://raw.githubusercontent.com/eric-mc2/test-viz/main/data/nodes.csv", index_col=False)
 edges = pd.read_csv("https://raw.githubusercontent.com/eric-mc2/test-viz/main/data/edges.csv", index_col=False)
 
-# # # I think hv edges need an actual integer source and target
-# nodes.index.name = 'idx'
-# nodes = nodes.reset_index()
-# edges['source_idx'] = edges['source'].map(nodes.set_index('id')['idx'])
-# edges['target_idx'] = edges['target'].map(nodes.set_index('id')['idx'])
-
 # Reposition LCC and non-LCC so they're not overlapping
 x_min, x_max = nodes['x'].min(), nodes['x'].max()
 x_pad = (x_max - x_min) * .1
 nodes.loc[nodes['lcc'] == 0, 'x'] += x_max + x_pad
 
-# Select properties to view
-nodes = nodes.drop(columns='category').rename(columns={'pretty_category':'category'})
+# Prettify property names
+nodes = nodes.rename(columns={'state_abbrv':'state'})
 
 # Create holoviews graph
-# hv_nodes = hv.Nodes(nodes, kdims=['x','y','idx'], vdims=['name','category','state_abbrv'])
-# hv_graph = hv.Graph(((edges['source_idx'], edges['target_idx'],edges['amount']), hv_nodes), vdims='amount')
-hv_nodes = hv.Nodes(nodes, kdims=['x','y','id'], vdims=['name','category','state_abbrv'])
+hv_nodes = hv.Nodes(nodes, kdims=['x','y','id'], vdims=['name','category','state'])
 hv_graph = hv.Graph(((edges['source'], edges['target'],edges['amount']), hv_nodes), vdims='amount')
 
 hv_opts = dict(
